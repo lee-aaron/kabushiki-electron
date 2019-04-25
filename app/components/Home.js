1d5ab3754e46e3c619d4d830e4514b8ec96a288d
lee-aaron <lee.aaron.68@gmail.com>
2019-04-24 17:25:31 -0700

    Update to make it more standalone

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,18 +3,25 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes';
 import styles from './Home.css';
+const remote = require('electron').remote;
 
 type Props = {};
 
 export default class Home extends Component<Props> {
   props: Props;
 
+  closeWindow() {
+    const window = remote.getCurrentWindow();
+    window.close();
+  }
+
   render() {
     return (
       <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
+        <h1>Welcome to Kabushiki Web!</h1>
+        <button id="close-btn" onClick={this.closeWindow} >X</button>
       </div>
     );
   }
 }
+
--- app/main.dev.js
+++ app/main.dev.js
@@ -70,8 +70,11 @@ app.on('ready', async () => {
   mainWindow = new BrowserWindow({
     show: false,
     width: 1024,
-    height: 728
+    height: 728,
+    frame: false
   });
+
+  mainWindow.maximize();
 
   mainWindow.loadURL(`file://${__dirname}/app.html`);
 
